fec4f2f30f360b2eeb4df8cf55e04e8f711aa715
Katia Ameri <katiaa@stanford.edu>
2014-02-20 18:42:25 -0800

    lab 7

--- models.js
+++ models.js
@@ -1,9 +1,14 @@
 
 var Mongoose = require('mongoose');
 
-
 var ProjectSchema = new Mongoose.Schema({
   // fields are defined here
+//my stuff
+"title": String,
+"date": Date,
+"summary": String,
+"image": String
+//end of my stuff
 });
 
 exports.Project = Mongoose.model('Project', ProjectSchema);
--- routes/project.js
+++ routes/project.js
@@ -2,6 +2,11 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
+  models.project
+  .find({"_id": projectID})
+  .sort('date')
+
+  .exec(afterQuery);
 
   // query for the specific project and
   // call the following callback
@@ -18,10 +23,35 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+
+  var newPost = new models.project ({ 
+    "title": form_data['project_title'],
+    "date": Date.parse(form_data['data']),
+    "image": form_data['image_url']
+
+  })
+
+  newPost.save(addCallback);
+
+  function addCallback(err) {
+    if(err) {console.log(err); res.send(500);}
+    res.send(200);
+  }
+  
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+
+  models.Project
+  .find({"_id": projectID})
+  .remove()
+  .exec(deleteCallback);
+
+  function deleteCallback(err, deleteBlogs) {
+    if(err) {console.log(err); res.send(500);}
+    res.send(200);
+  }
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
